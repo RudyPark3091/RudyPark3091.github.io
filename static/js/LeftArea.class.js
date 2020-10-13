502df1d938dbbb04e63d0c95a7678afa05ecf9f7
RudyPark3091 <grayblack313@gmail.com>
2020-10-13 14:35:49 +0900

    Fix

--- static/js/LeftArea.class.js
+++ static/js/LeftArea.class.js
@@ -6,14 +6,27 @@ class LeftArea {
       article: "Articles"
     };
     this.context[context] = "&lt" + this.context[context] + "&gt";
-    this.link = new Link();
 
     document.body.innerHTML += `
       <div id="left-area">
-        ${this.link.makeLink(this.context.home, "/")}
-        ${this.link.makeLink(this.context.memo, "/memo")}
-        ${this.link.makeLink(this.context.article, "/article")}
+        ${this.makeLink(this.context.home, "/")}
+        ${this.makeLink(this.context.memo, "/memo")}
+        ${this.makeLink(this.context.article, "/article")}
       </div>
     `;
+  }
+
+  htmlOp() {
+    document.body.innerHTML += `
+      <div id="left-area">
+        ${this.makeLink(this.context.home, "/")}
+        ${this.makeLink(this.context.memo, "/memo")}
+        ${this.makeLink(this.context.article, "/article")}
+      </div>
+    `;
+  }
+
+  makeLink(name, url) {
+    return `<a href=${url}>${name}</a>`;
   }
 }
