544c75291cd9f1b7b27d61a20239b832b3157d06
RudyPark3091 <grayblack313@gmail.com>
2020-10-14 17:05:19 +0900

    Add navigator buttons

--- static/js/Navigator.class.js
+++ static/js/Navigator.class.js
@@ -5,10 +5,16 @@ class Navigator {
 
   render() {
     return `
-    <div id="navigator" class="fadein">
+    <div id="navigator-bar" class="fadein">
       <div id="red" class="navigator-traffic-light"></div>
       <div id="yellow" class="navigator-traffic-light"></div>
       <div id="green" class="navigator-traffic-light"></div>
-    </div>`;
+    </div>
+    <div id="navigator-buttons" class="fadein">
+      <div id="navbuttons-back" class="navbuttons"></div>
+      <div id="navbuttons-forward" class="navbuttons"></div>
+      <div id="navbuttons-address" class="navbuttons"></div>
+    </div>
+    `;
   }
 }
